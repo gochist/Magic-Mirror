83eb031e24de547c3de46b242692428c88b24dae
gochi <gochi@gochi-laptop.(none)>
2010-06-30 12:51:44 +0900

    moved User class to fetchers

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,3 @@
-from models import UserModel
 import msg
 from datetime import datetime, timedelta
 
@@ -10,49 +9,3 @@ def utc_time(local_time_str, utcoffset=0):
 
 def local_time(utc_time, utcoffset=0):
     return utc_time + timedelta(seconds=utcoffset)
-
-class User(object):
-    def __init__(self):
-        self.loaded = False
-        self.model = None
-        
-    def get_by_twit_id(self, twit_id):
-        query = UserModel.all().filter("twit_id =", twit_id)
-        count = query.count()
-        if count == 1:
-            self.loaded = True
-            self.model = query.fetch(1)[0]
-        else:
-            self.loaded = False
-            self.model = None
-            
-        if count > 1:
-            msg.warn(1, twit_id=twit_id)            
-
-        return self.model
-
-    def set(self, twit_id, twit_screen_name, twit_img_url):
-        """set user information into DB
-        
-        Args: 
-          twit_id:
-          twit_screen_name:
-          twit_img_url:
-        """
-        model = self.get_by_twit_id(twit_id)
-        if model: # when data already exists  
-            model.twit_id = twit_id
-            model.twit_screen_name = twit_screen_name
-            model.twit_img_url = twit_img_url
-            model.put()
-        else :  # when data is not there
-            model = UserModel(twit_id=twit_id,
-                              twit_screen_name=twit_screen_name,
-                              twit_img_url=twit_img_url)
-            model.put()
-            
-        self.model = model
-        
-        return self.model
-        
-            
